52eef461f4e236d0081d3707715270c0271d56e6
siraaj-git <sqadir@smail.pembinatrails.ca>
2024-10-02 13:58:54 -0500

    add interaction to the game

--- game.sh
+++ game.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 echo "Welcome to the Adventure Game"
+echo "Choose a location to explore, the forest, castle or cave."
+read location
+
+if["$location" == "forest"]; then
+	cat forest.txt
+elif ["$location" == "castle"]; then
+	cat castle.txt
+elif ["$location" == "cave"]; then
+	cat cave.txt
+else 
+	echo "Invalid location"
+fi
